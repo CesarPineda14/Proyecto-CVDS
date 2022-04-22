118ce74bca3a495f33cf5517ea91bc17cd044f53
Yesid Carrillo <yesid.carrillo@mail.escuelaing.edu.co>
2022-04-21 22:12:10 -0500

    shiro

--- src/main/java/edu/eci/cvds/entities/prueba.java
+++ src/main/java/edu/eci/cvds/entities/prueba.java
@@ -1,12 +1,12 @@
-package edu.eci.cvds.entities;
+package edu.eci.cvds.entities;/*
 import edu.eci.cvds.Exceptions.BibliotecaException;
 import edu.eci.cvds.entities.Recurso;
 import edu.eci.cvds.persistence.PersistenceException;
 import edu.eci.cvds.services.ECILibraryServices;
 import edu.eci.cvds.services.impl.ECIBookServices;
-import edu.eci.cvds.services.impl.ECILibraryServicesImpl;
+import edu.eci.cvds.services.impl.ECILibraryServicesImpl;*/
 public class prueba {
-
+/*
         public static void main(String[] args) throws PersistenceException {
           ECIBookServices instance = ECILibraryServicesFactory.getInstance().getECILibraryServices();
 //        System.out.println(instance.consultarCliente(2165711).toString());
@@ -16,5 +16,5 @@ public class prueba {
             }
 
         }
-    }
+    }*/
 }
