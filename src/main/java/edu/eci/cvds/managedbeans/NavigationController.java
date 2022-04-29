f3f28396fb46638c35fa76899c291e4b5782e62f
unknown <edwar.lozano@labinfo.is.escuelaing.edu.co>
2022-04-29 09:25:43 -0500

    Avances conexiones entre html y back

---
+++ src/main/java/edu/eci/cvds/managedbeans/NavigationController.java
@@ -0,0 +1,24 @@
+package edu.eci.cvds.managedbeans;
+
+import java.io.Serializable;  
+
+import javax.faces.bean.ManagedBean; 
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "navigationController", eager = true)
+@RequestScoped
+
+public class NavigationController implements Serializable {
+    public String paginaInicio() {
+        return "inicioSesion";
+    }
+    public String paginaRegistroRec() {
+        return "registrarRecurso";
+    }
+    public String paginaConsultaRec() {
+        return "consultarRecursos";
+    }
+    public String paginaReservas() {
+        return "reservas";
+    }
+}
