bb0176a1c9631cc0f71085ea11371e67f8ae42bd
unknown <edwar.lozano@labinfo.is.escuelaing.edu.co>
2022-05-12 11:58:44 -0500

    Avances ventana de reservar recursos

--- src/main/java/edu/eci/cvds/managedbeans/CreateReserveBean.java
+++ src/main/java/edu/eci/cvds/managedbeans/CreateReserveBean.java
@@ -2,6 +2,9 @@ package edu.eci.cvds.managedbeans;
 
 
 import com.google.inject.Inject;
+
+import org.primefaces.event.SelectEvent;
+
 import edu.eci.cvds.entities.Reserve;
 import edu.eci.cvds.entities.Resource;
 import edu.eci.cvds.services.implementation.CiclasServices;
@@ -9,6 +12,8 @@ import edu.eci.cvds.services.implementation.CiclasServices;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 @ManagedBean(name = "createReserve")
 @RequestScoped
@@ -19,6 +24,8 @@ public class CreateReserveBean extends BasePageBean{
 
     private List<Resource> resourceList;
     private List<Reserve> reserveList;
+    private Date fecha;
+    private String fechaSelec = "";
 
 
     public CreateReserveBean() {
@@ -32,6 +39,22 @@ public class CreateReserveBean extends BasePageBean{
 
     }
 
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getFechaSelec() {
+        return fechaSelec;
+    }
+
+    public void setFechaSelec(String fechaSelec) {
+        this.fechaSelec = fechaSelec;
+    }
+
 
 
     public CiclasServices getEciBookServices() {
@@ -40,6 +63,13 @@ public class CreateReserveBean extends BasePageBean{
 
     public void setEciBookServices(CiclasServices ciclasServices) {
         this.ciclasServices = ciclasServices;
+    }
+
+    public void actualizarFecha(SelectEvent event) {
+        SimpleDateFormat fecha1 = new SimpleDateFormat("EEEEE dd MMMMM yyyy");
+        StringBuilder cadena_fecha1_11 = new StringBuilder(fecha1.format(event.getObject()));
+        fechaSelec = cadena_fecha1_11.toString();
+        
     }
 
 
