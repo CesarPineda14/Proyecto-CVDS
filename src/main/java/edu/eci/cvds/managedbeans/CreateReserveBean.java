4993f54b5dd73cdb7409ec610244c4586367fc0b
YesidCarrillo26 <yesid.carrillo@mail.escuelaing.edu.co>
2022-05-17 22:24:16 -0500

    create reserve full

--- src/main/java/edu/eci/cvds/managedbeans/CreateReserveBean.java
+++ src/main/java/edu/eci/cvds/managedbeans/CreateReserveBean.java
@@ -3,6 +3,7 @@ package edu.eci.cvds.managedbeans;
 
 import com.google.inject.Inject;
 
+import edu.eci.cvds.services.ExceptionServices;
 import org.primefaces.event.SelectEvent;
 
 import edu.eci.cvds.entities.Reserve;
@@ -10,9 +11,13 @@ import edu.eci.cvds.entities.Resource;
 import edu.eci.cvds.services.implementation.CiclasServices;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.persistence.criteria.CriteriaBuilder;
 
+import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -27,7 +32,30 @@ public class CreateReserveBean extends BasePageBean{
     private List<Reserve> reserveList;
     private Date fecha;
     private String fechaSelec = "";
+    private String selectedOneMenuRecurso;
+    private String selectedOneMenuUser;
+    private Date horaInicialSelected;
+    private Date horaFinalSelected;
+    private String periodicidad;
+    private Date fechaFinal;
 
+
+
+    public void createReserve() {
+        try {
+            Integer selectedOneMenuRecursoI = Integer.parseInt(selectedOneMenuRecurso);
+            Integer selectedOneMenuUserI = Integer.parseInt(selectedOneMenuUser);
+            Time horaInicialT = new Time (horaInicialSelected.getTime());
+            Time horaFinalT = new Time (horaFinalSelected.getTime());
+            Reserve reserve = new Reserve(selectedOneMenuRecursoI, selectedOneMenuUserI, horaInicialT, horaFinalT,
+                    periodicidad, new java.sql.Date(fechaFinal.getTime()));
+            addMessage("Confirmed", "Reserva Agregada");
+            ciclasServices.createReserve(reserve);
+        } catch (Exception e){
+            addMessage("Not Confirmed", "Reserva No Agregada");
+            System.out.println(e.getMessage());
+        }
+    }
 
     public CreateReserveBean() {
 
@@ -40,12 +68,66 @@ public class CreateReserveBean extends BasePageBean{
 
     }
 
-
     public void actualizarFecha(SelectEvent event) {
         SimpleDateFormat fecha1 = new SimpleDateFormat("EEEEE dd MMMMM yyyy");
         StringBuilder cadena_fecha1_11 = new StringBuilder(fecha1.format(event.getObject()));
         fechaSelec = cadena_fecha1_11.toString();
         
+    }
+
+    public void addMessage(String summary, String detail) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, detail);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+    public Date getHoraInicialSelected() {
+        return horaInicialSelected;
+    }
+
+    public void setHoraInicialSelected(Date horaInicialSelected) {
+        this.horaInicialSelected = horaInicialSelected;
+    }
+
+    public Date getHoraFinalSelected() {
+        return horaFinalSelected;
+    }
+
+    public void setHoraFinalSelected(Date horaFinalSelected) {
+        this.horaFinalSelected = horaFinalSelected;
+    }
+
+    public String getSelectedOneMenuRecurso() {
+        return selectedOneMenuRecurso;
+    }
+
+    public void setSelectedOneMenuRecurso(String selectedOneMenuRecurso) {
+        this.selectedOneMenuRecurso = selectedOneMenuRecurso;
+    }
+
+    public String getSelectedOneMenuUser() {
+        return selectedOneMenuUser;
+    }
+
+    public void setSelectedOneMenuUser(String selectedOneMenuUser) {
+        this.selectedOneMenuUser = selectedOneMenuUser;
+    }
+
+
+
+    public String getPeriodicidad() {
+        return periodicidad;
+    }
+
+    public void setPeriodicidad(String periodicidad) {
+        this.periodicidad = periodicidad;
+    }
+
+    public Date getFechaFinal() {
+        return fechaFinal;
+    }
+
+    public void setFechaFinal(Date fechaFinal) {
+        this.fechaFinal = fechaFinal;
     }
 
 
